062b6eb946704b458a78b96cb21489bf11785f26
wendyzqw <37916754+wendyzqw@users.noreply.github.com>
2023-07-10 05:52:46 +0800

    Create multi_layer_net.py

---
+++ multi_layer_net.py
@@ -0,0 +1,112 @@
+import sys, os
+sys.path.append(os.pardir)
+import pickle
+import numpy as np
+from collections import OrderedDict
+from util import *
+
+class MultiLayerNet:
+    def __init__(self, input_size, hidden_size_list, output_size,
+                 activation='relu', weight_init_std='relu', l2_lambda=0):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size_list = hidden_size_list
+        self.hidden_layer_num = len(hidden_size_list)
+        self.l2_lambda = l2_lambda
+        self.params = {}
+        self.__init_weight(weight_init_std)
+
+        activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
+        self.layers = OrderedDict()
+        for idx in range(1, self.hidden_layer_num+1):
+            self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+                                                      self.params['b' + str(idx)])
+            self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
+
+        idx = self.hidden_layer_num + 1
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+            self.params['b' + str(idx)])
+
+        self.last_layer = SoftmaxWithLoss()
+
+    def __init_weight(self, weight_init_std):
+        """
+        初始化权重
+        """
+        all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1])
+            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1])
+
+            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx-1], all_size_list[idx])
+            self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        """
+
+        :param x: 输入
+        :param t: 目标值
+        :return: loss
+        """
+        y = self.predict(x)
+
+        weight_decay = 0
+        for idx in range(1, self.hidden_layer_num + 2):
+            W = self.params['W' + str(idx)]
+            weight_decay += 0.5 * self.l2_lambda * np.sum(W ** 2)
+
+        return self.last_layer.forward(y, t) + weight_decay
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 计算和保存梯度
+        grads = {}
+        for idx in range(1, self.hidden_layer_num+2):
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.l2_lambda * self.layers['Affine' + str(idx)].W
+            grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+
+        return grads
+
+    def save_params(self, file_names="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_names, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name='params.pkl'):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i + 1)]
+            self.layers[key].b = self.params['b' + str(i + 1)]
